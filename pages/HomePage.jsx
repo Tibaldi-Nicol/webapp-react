b34556b1d476483ccfd455d3fbe9a2886cc185ac
Tibaldi-Nicol <nickkoltibaldi@gmail.com>
2025-06-03 19:53:30 +0200

    [UPD]Add-homepage

---
+++ pages/HomePage.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Homepage=()=>{
+    return(
+        <>
+        <h1 className="text-primary">
+            MOVIE MOVES
+        </h1>
+
+        <h2><i>The Movies List</i></h2>
+
+        <div className="row">
+            {/**inserisco i film */}
+        </div>
+
+        </>
+    )
+}
+export default Homepage;
