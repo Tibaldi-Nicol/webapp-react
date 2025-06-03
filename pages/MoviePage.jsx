c5e9a952a3db776a2be2c1ec3bc0b46b14640325
Tibaldi-Nicol <nickkoltibaldi@gmail.com>
2025-06-03 19:53:53 +0200

    [UPD]Add-MoviePage

---
+++ pages/MoviePage.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+const MoviePage =()=>{
+    return(
+
+        <div className="row">
+
+            <div className="col-12 col-md-6 col-lg-4">
+                immagine
+            </div>
+
+             <div className="col-12 col-md-6 col-lg-8">
+                <h1>titolo</h1>
+                <p>altre informazioni</p>
+            </div>
+
+
+        </div>
+
+
+
+
+    );
+}
+
+export default MoviePage
