e6218f5e6fa8d83a7660224897d93b16cf10e458
Tibaldi-Nicol <nickkoltibaldi@gmail.com>
2025-06-03 21:29:25 +0200

    [UPD]Add-details-moviepage

--- webapp-react/src/App.jsx
+++ webapp-react/src/App.jsx
@@ -1,5 +1,8 @@
 import DefaultLayout from "./layout/Defaultlayout"; // Corretto il nome dell'import
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Homepage from "../../pages/HomePage";
+import MoviePage from "../../pages/MoviePage";
+import NotFoundPages from "../../pages/NotFoundPages";
 
 function App() {
   return (
@@ -8,7 +11,9 @@ function App() {
         {/* Definisce la route con il layout */}
         <Route element={<DefaultLayout />}> 
           {/* Definisce la route principale (Homepage) */}
-          <Route index element={<h1>Homepage</h1>} />
+          <Route index element={<Homepage/>} />
+          <Route path="/movies/:id" element={<MoviePage/>}></Route>
+          <Route path="*" element={<NotFoundPages/>}></Route>
         </Route>
       </Routes>
     </BrowserRouter>
