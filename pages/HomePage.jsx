2154118cd1051b6c80402a6ada26fb8e8ada5b5f
Tibaldi-Nicol <nickkoltibaldi@gmail.com>
2025-06-03 21:20:02 +0200

    [UPD]Add-card

--- pages/HomePage.jsx
+++ pages/HomePage.jsx
@@ -1,19 +1,39 @@
 import React from "react";
 
-const Homepage=()=>{
-    return(
-        <>
-        <h1 className="text-primary">
-            MOVIE MOVES
-        </h1>
+const Homepage = () => {
+  return (
+    <>
+      {/* Titolo principale */}
+      <h1 className="text-primary text-center">MOVIE MOVES</h1>
+      <h2 className="text-center">
+        <i>The Movies List</i>
+      </h2>
 
-        <h2><i>The Movies List</i></h2>
-
-        <div className="row">
-            {/**inserisco i film */}
+      {/* Sezione per i film */}
+      <div className="container mt-4">
+        <div className="row justify-content-center">
+          {/* Card del film */}
+          <div className="col-12 col-md-6 col-lg-4">
+            <div className="card">
+              <img
+                src="https://picsum.photos/200/300"
+                alt="Movie"
+                className="card-img-top"
+              />
+              <div className="card-body text-center">
+                <h3 className="text-primary">Titolo del Film</h3>
+                <h4><em>Autore</em></h4>
+                <p>Descrizione breve del film.</p>
+                <a href="#" className="btn btn-primary">
+                  Scopri di più
+                </a>
+              </div>
+            </div>
+          </div>
         </div>
+      </div>
+    </>
+  );
+};
 
-        </>
-    )
-}
 export default Homepage;
